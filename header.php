8d27dbd753091017503e9d43a1d2c067f3ecac6f
marklippert <lippert@gmail.com>
2017-03-13 11:12:20 -0500

    Change Google Analytics account

--- header.php
+++ header.php
@@ -45,7 +45,7 @@ function email($address, $name="") {
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-93113916-1', 'auto');
+      ga('create', 'UA-93567331-1', 'auto');
       ga('send', 'pageview');
     </script>
   </head>
