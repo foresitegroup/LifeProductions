88443885c6958f9d318569ee8468a9882476c800
foresitegroup <mark@foresitegrp.com>
2022-07-12 09:49:45 -0500

    Add new analytics

--- header.php
+++ header.php
@@ -38,15 +38,24 @@ function email($address, $name="") {
         });
       });
     </script>
-
+    
+    <!-- This will stop working July 1, 2023 -->
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
       ga('create', 'UA-93567331-1', 'auto');
       ga('send', 'pageview');
+    </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-524W8F3YZ6"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-524W8F3YZ6');
     </script>
   </head>
   <body>
